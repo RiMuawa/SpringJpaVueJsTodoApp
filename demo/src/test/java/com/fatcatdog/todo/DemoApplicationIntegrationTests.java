4d5bab300bd55c7dc48bb7b41e9449785341cfd8
RiMuawa <shenlilinghuaddog1@gmail.com>
2025-06-05 11:04:48 +0800

    Replace JPA with JDBC layer

--- demo/src/test/java/com/fatcatdog/todo/DemoApplicationIntegrationTests.java
+++ demo/src/test/java/com/fatcatdog/todo/DemoApplicationIntegrationTests.java
@@ -3,7 +3,6 @@ package com.fatcatdog.todo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
